d5c302d2742cfb6834c7d3d3291c3c4982a3e75f
Yvanohe <yvanohe@gmail.com>
2024-04-24 17:16:16 +0200

    add @operation annotation on NauticalActivityController for Swagger

--- src/main/java/fr/lubac/surfouAPI/controller/NauticalActivityController.java
+++ src/main/java/fr/lubac/surfouAPI/controller/NauticalActivityController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fr.lubac.surfouAPI.model.NauticalActivity;
 import fr.lubac.surfouAPI.service.NauticalActivityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name="Nautical activity", description="api for nautical activities associated with the location (spot) and compatible weather conditions")
@@ -35,6 +37,13 @@ public class NauticalActivityController {
 	 * @param params optional request parameters : wforce for wind force (knots kn) and wdir for wind direction (unity ° between 0 and 360) to filter nautical activities bases compatibilities with associated weather conditions
 	 * @return An Iterable object of Nautical activity object full filled
 	 */
+	 @Operation(
+	            summary = "Fetch all nautical activities",
+	            description = "Fetch all nautical activities and their data (spot, activity description, weather xondition) from database. Possibility to filter by weather condition.",
+	            parameters = {
+	            		@Parameter(name ="wforce", required=false, description = "Wind force in Knots to filter nautical activities compatible with this wind force"),
+	            		@Parameter(name ="wdir", required=false, description = "Wind direction in degree (between 0 & 360°) to filter nautical activities compatible with this wind direction")
+	            })
 	@GetMapping("/nauticalactivities")
 	public Iterable<NauticalActivity> getNauticalActivitiesByWindcompatibility (@RequestParam(required = false) Map<String, String> params ) {		
 		 if (params.containsKey("wforce") && params.containsKey("wdir")) {
