f18c178b9afb21ce247ddd3ce0a27af68fbb432a
Yvanohe <yvanohe@gmail.com>
2024-04-27 18:40:21 +0200

    adding response code for weather condition  creation

--- src/main/java/fr/lubac/surfouAPI/controller/WeatherConditionController.java
+++ src/main/java/fr/lubac/surfouAPI/controller/WeatherConditionController.java
@@ -1,13 +1,17 @@
 package fr.lubac.surfouAPI.controller;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import fr.lubac.surfouAPI.model.WeatherCondition;
 import fr.lubac.surfouAPI.service.WeatherConditionService;
@@ -28,8 +32,18 @@ public class WeatherConditionController {
 	 * @return saved WeatherCondition object
 	 */
 	@PostMapping
-	public WeatherCondition createWeatherCondition (@RequestBody WeatherCondition weatherCondition) {
-		return weatherConditionService.saveWeatherCondition(weatherCondition);
+	public ResponseEntity<WeatherCondition> createWeatherCondition (@RequestBody WeatherCondition weatherCondition) {
+		WeatherCondition newWeatherConditionAdded = weatherConditionService.saveWeatherCondition(weatherCondition);
+		if (newWeatherConditionAdded == null) {
+			return ResponseEntity.noContent().build();	
+		} else {
+			URI location = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(newWeatherConditionAdded.getId())
+					.toUri();
+			return ResponseEntity.created(location).build();
+		}	
 	}
 	
 	@GetMapping
@@ -37,5 +51,9 @@ public class WeatherConditionController {
 		return weatherConditionService.getWeatherConditions();
 	}	
 	
+	@GetMapping("/{id}")
+	public Optional<WeatherCondition> getWeatherCondition(@PathVariable("id") int id) {
+		return weatherConditionService.getWeatherCondition(id);
+	}
 	
 }
